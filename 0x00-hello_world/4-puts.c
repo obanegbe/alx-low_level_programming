e9df8ad081a862d834c6c67e07eca7d0ea07e137
obanegbe <oluwatobi.obanegbe"gmail.com>
2022-09-08 19:35:48 -0700

    task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+
+
+/**
+ *
+ *  * main - prints a simple stirng
+ *
+ *   * Return: int 0 for success
+ *
+ *    */
+
+int main(void)
+
+{
+
+		puts("\"Programming is like building a multilingual puzzle");
+
+			return (0);
+
+}
