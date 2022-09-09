d9f54a862e98b54ba5d69d50f6e428bf2e25e3f7
obanegbe <oluwatobi.obanegbe"gmail.com>
2022-09-08 19:42:55 -0700

    task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+
+
+/**
+ *
+ *  * main - prints a simple message
+ *
+ *   * Return: 0 for success
+ *
+ *    */
+
+
+
+int main(void)
+
+{
+
+		printf("with proper grammar, but the outcome is a piece of art,\n");
+
+			return (0);
+
+}
