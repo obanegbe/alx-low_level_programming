bdb76e49a4361848363ef5f3af6538d64acf67d9
obanegbe <oluwatobi.obanegbe"gmail.com>
2022-09-09 20:43:07 -0700

    task 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,52 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ *
+ *  * main - prints letters of the alphabet
+ *
+ *   * Return: 0 for success
+ *
+ *   */
+
+int main(void)
+
+{
+
+		char c = 'a';
+
+			char d = 'A';
+
+
+
+				while (c <= 'z')
+
+						{
+
+									putchar(c);
+
+											c++;
+
+												}
+
+
+
+					while (d <= 'Z')
+
+							{
+
+										putchar(d);
+
+												d++;
+
+														}
+
+
+
+						putchar('\n');
+
+
+
+							return (0);
+
+}
