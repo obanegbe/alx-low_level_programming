239b0d88b5b225501fe31819e4200cfd9b771210
obanegbe <oluwatobi.obanegbe"gmail.com>
2022-09-09 21:01:07 -0700

    task 8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+
+
+
+/**
+ *
+ *  * main - prints letters of the alphabet
+ *
+ *   * Return: 0 for success
+ *
+ *    */
+
+int main(void)
+
+{
+
+		char c = 'a';
+
+			int n = '0';
+
+
+
+				while (n <= '9')
+
+						{
+
+									putchar(n);
+
+											n++;
+
+												}
+
+
+
+					while (c <= 'f')
+
+							{
+
+										putchar(c);
+
+												c++;
+
+													}
+
+
+
+						putchar('\n');
+
+
+
+							return (0);
+
+}
