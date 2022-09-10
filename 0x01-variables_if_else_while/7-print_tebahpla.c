e6f255306fd4a0439d702abb61c0a1f6e4474b03
obanegbe <oluwatobi.obanegbe"gmail.com>
2022-09-09 20:58:29 -0700

    task 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ *
+ *  * main - prints letters of the alphabet in reverse order
+ *
+ *   * Return: 0 for success
+ *
+ *    */
+int main(void)
+{
+		char c = 'z';
+			while (c >= 'a')
+					{
+								putchar(c);
+										c--;
+											}
+				putchar('\n');
+					return (0);
+
+}
