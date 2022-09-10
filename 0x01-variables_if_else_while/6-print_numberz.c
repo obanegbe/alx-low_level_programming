c0f048ac52c7bd9e43f653b2fd2aabcb8ddf4103
obanegbe <oluwatobi.obanegbe"gmail.com>
2022-09-09 20:53:54 -0700

    task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,41 @@
+#include <stdlib.h>
+
+#include <stdio.h>
+
+
+
+/**
+ *
+ *  * main - prints letters of the alphabet
+ *
+ *   * Return: 0 for success
+ *
+ *    */
+
+int main(void)
+
+{
+
+		int x = '0';
+
+
+
+			while (x <= '9')
+
+					{
+
+								putchar(x);
+
+										x++;
+
+											}
+
+
+
+				putchar('\n');
+
+
+
+					return (0);
+
+}
