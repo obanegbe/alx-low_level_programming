235af6da02c362a67ea1bd65f420881153011932
obanegbe <oluwatobi.obanegbe"gmail.com>
2022-09-09 20:52:23 -0700

    task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+
+
+/**
+ *
+ *  * main - prints numbers 0 through 9
+ *
+ *   * Return: 0 for success
+ *
+ *    */
+
+int main(void)
+
+{
+
+		int x = 0;
+
+
+
+			while (x <= 9)
+
+					{
+
+								printf("%d", x);
+
+										x++;
+
+											}
+
+
+
+				putchar('\n');
+
+
+
+					return (0);
+
+}
